b575b29ea2b416e2cface42a880fd48e2e322f15
Gump0 <blenderjack2004@gmail.com>
2024-06-06 20:24:57 -0400

    implemented sample portrait image and trimmed any unnesessary bloated code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.content-button');
     const contents = document.querySelectorAll('.content');
